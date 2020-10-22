4438e2f8b0dd007d4faec6ea99280986c86bc34d
Fang Ye <yef10@mcmaster.ca>
2020-10-22 15:43:51 -0400

    add data for prototype 1.0

---
+++ community_front/src/data.js
@@ -0,0 +1,116 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            category: 'Products',
+            name: 'Birthday Cake',
+            image: '/images/product_cake.jpg',
+            price: 20,
+            quantity: 20,
+            rating: 5,
+            description: "Homebaked Cakes - ideal for Birthdays and Special Occasions. ",
+            city: "Hamilton",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '2',
+            category: 'Products',
+            name: 'Cookies',
+            image: '/images/product_cookies.jpg',
+            price: 3,
+            quantity: 20,
+            rating: 4.5,
+            description: " ",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '3',
+            category: 'Products',
+            name: 'Cute Cookies',
+            image: '/images/product_candy.jpg',
+            price: 4,
+            quantity: 20,
+            rating: 4.5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+    ],
+    services: [
+        {
+            _id: '1',
+            category: 'services',
+            name: 'plumber',
+            image: '/images/service_plumber.jpg',
+            price:20,
+            quantity: 20,
+            rating: 5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '2',
+            category: 'services',
+            name: 'Air Conditioner Repair',
+            image: '/images/service_airConditionerRepair.jpg',
+            price:20,
+            quantity: 20,
+            rating: 5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '3',
+            category: 'services',
+            name: 'Heating Maintenance',
+            image: '/images/service_heatingMaintance.jpg',
+            price:20,
+            quantity: 20,
+            rating: 5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+    ],
+    competences: [
+        {
+            _id: '1',
+            category: 'Competences',
+            name: 'Food Deliver',
+            image: '/images/competence_deliver.jpg',
+            price: 19,
+            quantity: 20,
+            rating: 5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '2',
+            category: 'Competences',
+            name: 'Lawn Mowing',
+            image: '/images/competence_mowLawn.jpeg',
+            price: 18,
+            quantity: 20,
+            rating: 4.5,
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+        {
+            _id: '3',
+            category: 'Competences',
+            name: 'Sweep Snow',
+            image: '/images/competence_sweepSnow.jpg',
+            price: 19,
+            quantity: 20,
+            rating: 4.5, 
+            description: "",
+            city: "Toronto",
+            address: "100 Main St. S. Toronto ON L3P5H8"
+        },
+    ]
+}
