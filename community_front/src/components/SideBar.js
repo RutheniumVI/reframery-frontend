351ec22d562c72ffa61e270f7b46228fda6c015b
liyang wang <liyang.wang@ibm.com>
2020-10-26 16:46:36 +0800

    profile pages

--- community_front/src/components/SideBar.js
+++ community_front/src/components/SideBar.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function SideBar() {
   return (
     <div className="sidebar">
       <ul>
         <li>My Wallet</li>
         <li>My Items</li>
-        <li>My Profile</li>
-        <li>My History</li>
+        <Link to="./my-item">
+          <li>My Items</li>
+        </Link>
+        <Link to="./user-profile">
+          <li>My Profile</li>
+        </Link>
       </ul>
     </div>
   );
