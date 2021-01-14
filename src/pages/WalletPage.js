278647e718a1002b3334d5e192d6fa87c26953cf
liangw27 <liangw27>
2021-01-14 12:30:04 +0800

    wallet page updated

--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useData } from "../data/useData";
-
+import SideBar from "../components/SideBar";
+import { Link } from "react-router-dom";
+import Footer from '../components/Footer';
 
 
 function Currency() {
     return (
-        <div>
-            <h3>Your current balance: </h3>
+        <div >
+            <h3>Your current balance</h3>
             <div className="currency-box">
-                <h1>RCC$ 40</h1>
+                <h1>RCC$ 25</h1>
             </div>
         </div>
     );
@@ -18,7 +20,7 @@ function TransactionHistory() {
     const { data } = useData();
     return (
         
-            <div>
+            <div >
                 <h3>Past Transactions</h3>
                 <table className="transaction">
                     {data.products.map((product) => (
@@ -28,7 +30,12 @@ function TransactionHistory() {
                     ))}
 
                 </table>
+                <div className = "button-more">
+                    <button className="view-more">
+                        <Link to = "/myreframery/records"><h4>View more transactions</h4></Link>
+                    </button>
                 </div>
+            </div>
           
         
             );
@@ -36,13 +43,24 @@ function TransactionHistory() {
 
 function Main() {
         return (
-            <div className="wallet-page">
-                My Reframery {">"} My Wallet
-                <Currency />
-                <TransactionHistory />
+            <div>
+                <div className = "back-to-reframery">
+                    <Link to = "/">My Reframery</Link> {">>>"} My Wallet
+                </div>
+                <div className = "wallet">
+                    <div>
+                        <SideBar />
+                    </div>
+                    <div className="wallet-page">
+                        <Currency />
+                        <TransactionHistory />
+                    </div>
+                </div>
+                <div>
+                    <Footer />
+                </div>
             </div>
-
         );
     }
 
-    export default Main;
+export default Main;
